297fd727a6085adc9e02c9b16d3d61e5dab7de82
louvkii <lv@nuvopoint.dk>
2018-05-16 11:53:42 +0200

    Added Cat rep and interface

---
+++ DesktopWarrior/DAL/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using DesktopWarrior.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopWarrior.DAL.Interfaces
+{
+    public interface ICategoryRepository : IDisposable
+    {
+        List<Category> GetCategories();
+        Category GetCategoryById(int categoryId);
+    }
+}
