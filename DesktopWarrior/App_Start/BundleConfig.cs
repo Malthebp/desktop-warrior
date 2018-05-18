6a2c42705e49f0bd0e5e0cb43bb21b91794fa136
louvkii <0Elfonfire@gmail.com>
2018-05-18 15:12:02 +0200

    Set up header description thing

--- DesktopWarrior/App_Start/BundleConfig.cs
+++ DesktopWarrior/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace DesktopWarrior
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/accordion.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
---
+++ DesktopWarrior/Scripts/accordion.js
@@ -0,0 +1,20 @@
+﻿
+var acc = document.getElementsByClassName("accordion-btn");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        if (this.classList.contains("active")) {
+            $(event.target).html("Close  ▲");
+        } else {
+            $(event.target).html("Open   ▼");
+         }
+        var panel = document.getElementsByClassName('accordion-content')[0];
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        } 
+    });
+}
